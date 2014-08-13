e2c0f7d995c6af71b919f5c2ab07bc2b5a375ffa
Dharani Manne <dharani.manne@gmail.com>
2014-08-13 20:09:03 +0530

    basic structure of river crossing is done

--- RiverCrossing.java
+++ RiverCrossing.java
@@ -1,1 +1,32 @@
-public static void 
+import java.lang.*;
+import java.util.*;
+
+public class RiverCrossing
+{
+  boolean man;
+  boolean goat;
+  boolean cabbage;
+  boolean lion;
+
+  public RiverCrossing( boolean man, boolean goat, boolean cabbage, boolean lion )
+  {
+    man=man;
+    goat=goat;
+    cabbage=cabbage;
+    lion=lion;
+  }
+
+  public boolean isValid()
+  {
+    if (man==goat) 
+    {
+      return false;
+    }
+    else return true;
+  }
+
+  public boolean isGoal()
+  {
+    return man && goat && cabbage && lion;
+  }
+}
