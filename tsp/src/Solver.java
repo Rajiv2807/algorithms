3cabbbe7cbb1f74d30f88187a03e828a2de413e8
Dharani Manne <dharani.manne@gmail.com>
2014-09-23 19:49:31 +0530

    added comments

---
+++ tsp/src/Solver.java
@@ -0,0 +1,19 @@
+
+/***********************************************
+ * This is the solver class.
+ * It takes in the input from stdIn.
+ * It generates the city objects from stdIn.
+ * It runs the algorithms to solve the TSP.
+
+ @author : Dharani Manne
+ @author : Goutam Rajiv
+***********************************************/
+
+public class Solver
+{
+  //Rajiv, please do write the code to create city objects over here, reading from the stdIn.  
+  //If you want to maintain a 2D matrix for distances, we can do that here.
+  //Else, we can keep a 1D matrix in each city object.
+  
+  //Complete this soon so that we can finish the greedy algorithm by tonight.
+}
