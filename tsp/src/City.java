88694c978548f5381f70425790319d3bfb15eff8
Dharani Manne <dharani.manne@gmail.com>
2014-09-23 17:23:03 +0530

    added test case and method to give distances to a city

--- tsp/src/City.java
+++ tsp/src/City.java
@@ -1,16 +1,21 @@
-
+import java.util.Arrays;
 
 public class City{
     
   private int id;                                  // Each city has a unique id.
   private double x;                                // x coordinate of the city
   private double y;                                // y coordinate of the city
-//  private double distanceTo[];                     // array containing distances of a city from other cities
+  private double distanceTo[];                     // array containing distances of a city from other cities
 
   public City(int id, double x, double y){
     this.id = id;
     this.x = x;
     this.y = y;
+  }
+
+  public void assignDistanceTo(double[] distanceTo)
+  {
+    this.distanceTo = Arrays.copyOf(distanceTo, distanceTo.length);
   }
 
 }
