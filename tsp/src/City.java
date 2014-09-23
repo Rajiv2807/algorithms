7b3fd295121d01d0fa1c20bd35d4978b6d360625
Dharani Manne <dharani.manne@gmail.com>
2014-09-23 17:10:48 +0530

    started writing the source code, City data structure

---
+++ tsp/src/City.java
@@ -0,0 +1,16 @@
+
+
+public class City{
+    
+  private int id;                                  // Each city has a unique id.
+  private double x;                                // x coordinate of the city
+  private double y;                                // y coordinate of the city
+//  private double distanceTo[];                     // array containing distances of a city from other cities
+
+  public City(int id, double x, double y){
+    this.id = id;
+    this.x = x;
+    this.y = y;
+  }
+
+}
