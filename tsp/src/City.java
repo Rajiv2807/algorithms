8e152aea6c96b51cc28903e09e28ed5a7be1f2ae
Dharani Manne <dharani.manne@gmail.com>
2014-09-23 20:02:07 +0530

    modified and added algorithm for Solver

--- tsp/src/City.java
+++ tsp/src/City.java
@@ -1,18 +1,25 @@
 import java.util.Arrays;
 
-public class City{
-    
-  private int id;                                  // Each city has a unique id.
-  private double x;                                // x coordinate of the city
-  private double y;                                // y coordinate of the city
-  private double distanceTo[];                     // array containing distances of a city from other cities
+public class City
+{
+  
+  // Each city has a unique id.
+  private final int id;                                  
+  // x coordinate of the city
+  private double x;                                
+  // y coordinate of the city
+  private double y;                                
+  // array containing distances of this city from other cities
+  private double distanceTo[];                     
 
-  public City(int id, double x, double y){
+  public City(int id, double x, double y)
+  {
     this.id = id;
     this.x = x;
     this.y = y;
   }
 
+  //This method can be eliminted if we are using a 2D matrix to store distance information.
   public void assignDistanceTo(double[] distanceTo)
   {
     this.distanceTo = Arrays.copyOf(distanceTo, distanceTo.length);
--- tsp/src/Solver.java
+++ tsp/src/Solver.java
@@ -9,11 +9,30 @@
  @author : Goutam Rajiv
 ***********************************************/
 
+/*****************************************************************************************
+ * Algorithm:
+ *
+ * We maintain a open boolean list, which tells us if a city is visited or not.
+ * We start with a city, generate children, pick the best child and move to it.
+ * Best Child => Child with least distance from parent.
+ * We should try to optimize the moveGen() function so that less children are generated.
+ * Typical moveGen() function will be to generate all nodes which are not visited.
+ * We keep moving, while saving the order of cities in another list.
+ * Instead of 2 lists, we can have a list which stores the id of cities visited.
+ * This list will give us the order in which the cities are visited.
+ * The algorithm terminates if all cities are visited.
+*****************************************************************************************/
+
 public class Solver
 {
-  //Rajiv, please do write the code to create city objects over here, reading from the stdIn.  
-  //If you want to maintain a 2D matrix for distances, we can do that here.
-  //Else, we can keep a 1D matrix in each city object.
+
+  public Solver()
+  {
+    //Rajiv, please do write the code to create city objects over here, reading from the stdIn.  
+    //If you want to maintain a 2D matrix for distances, we can do that here.
+    //Else, we can keep a 1D matrix in each city object.
   
-  //Complete this soon so that we can finish the greedy algorithm by tonight.
+    //Complete this soon so that we can finish the greedy algorithm by tonight.
+  }
+
 }
